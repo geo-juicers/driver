bd71d833660461e156eeaf2cdf42af37f7c26c29
Cybergenik <cybergenik@gmail.com>
2023-03-25 04:13:23 -0600

    testing 2009-2021

--- src/main.rs
+++ src/main.rs
@@ -55,13 +55,16 @@ fn get_state(state: &str) -> u8 {
         _ => 0,
     }
 }
+
 fn main() {
     let api_key:String = dotenv::var("API_KEY").unwrap();
-    for year in vec!["2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"].iter() {
-        //let states: Vec::<api::State> = api::get_states_acs(&api_key, year).unwrap();
-        api::get_states_acs(&api_key, year).unwrap();
-        println!("{}", year)
-    }
+    let states: Vec::<api::State> = api::get_states_acs(&api_key, "2021").unwrap();
+    println!("{:#?}", states);
+    //for year in vec!["2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"].iter() {
+    //    //let states: Vec::<api::State> = api::get_states_acs(&api_key, year).unwrap();
+    //    api::get_states_acs(&api_key, year).unwrap();
+    //    println!("{}", year)
+    //}
     //for state in states {
     //    println!("{}: {}", state.name, state.pop.unwrap().total.unwrap());
     //}
